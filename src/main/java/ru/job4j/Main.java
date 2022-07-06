e868274b6f3bb2bd1c912892f3d639e4f00fc0b6
vegas76 <mas76@mail.ru>
2022-07-06 12:29:56 +0300

    6. Checkstyle [#4123 #318997]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,8 +2,8 @@ package ru.job4j; //заголовок всегда начинается package
 
 // тело кода  -связано с файлом Maim.java
 public class Main { // тело класса Main состоящее из методов (операторов)
-    @SuppressWarnings("checkstyle:MethodParamPad")
-    public static void main (String[] args) { // метод main
+
+    public static void main(String[] args) { // метод main
         System.out.println("Hello world"); // неделимый оператор
     }
 }
