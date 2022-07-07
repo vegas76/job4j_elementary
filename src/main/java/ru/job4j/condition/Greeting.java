9d2fc7b5754db4964b445bf87224f97bc593be25
vegas76 <mas76@mail.ru>
2022-07-07 16:32:10 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -6,7 +6,7 @@ public class Greeting {
         System.out.println(idea);
         idea +=  "But I am a newbie.";
         int year = 2022;
-        idea = idea + " " + year;
+        idea = idea + year;
         System.out.println(idea);
 
     }
