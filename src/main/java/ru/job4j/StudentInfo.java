914812a2654f8d5c199351a3a5607b3cc0e73709
vegas76 <mas76@mail.ru>
2022-07-08 09:01:09 +0300

    4.1. Разбор первой программы. [#159033 #318931]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,5 +1,5 @@
 package ru.job4j;
-    public class StudentInfo {
+public class StudentInfo {
         public static void main(String[] args) {
             System.out.println("Igor Masilyuk");
             System.out.println("05.02.1976");
